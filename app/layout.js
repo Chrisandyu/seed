73fdc57e77e5a729581aea54b08bc7fbd91328a3
komorebii <christopherzhang1111@gmail.com>
2025-07-13 23:21:21 -0700

    MORE

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({
@@ -14,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
